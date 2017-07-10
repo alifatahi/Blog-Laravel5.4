c704aada516321554f812f0851bea70d96e2c9a3
Ali Fatahi <a.fatahi1992@gmail.com>
2017-07-10 23:08:08 +0430

    Create Comment Model For add comment to our post

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -6,5 +6,19 @@
             <article>
                 {{$post->body}}
             </article>
+        <hr>
+
+        <div class="comments">
+            <ul class="list-group">
+                @foreach($post->comments as $comment)
+                        <li class="list-group-item">
+                            {{--<strong>--}}
+                                {{--{{$comment->created_at->diffsForHumans()}} : &nbsp;--}}
+                            {{--</strong>--}}
+                            {{$comment->body}}
+                        </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 @endsection
