a10cb255abef6a41b9d85c9e645656e85b633843
Ali Fatahi <a.fatahi1992@gmail.com>
2017-07-12 14:52:11 +0430

    we make View Composer for archives to be accessable anywhere on site

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Post;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,14 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        /*
+        so we simply say we want to load view sidebar and also we want to
+        bind value to it which is archive
+        */
+        view()->composer('layouts.sidebar',function ($view){
+                $view->with('archives',Post::archives());
+        });
     }
 
     /**
